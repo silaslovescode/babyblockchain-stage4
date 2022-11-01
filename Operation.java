4570397d3a945029dc2e8c7080d9b676dc12b2df
silas magu <107644537+silaslovescode@users.noreply.github.com>
2022-11-01 17:01:40 +0300

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,21 @@
+package BABY-BLOCKCHAIN;
+
+import Integer;
+import java.util.Vector;
+
+public class Operation extends Account, , Signature, Account {
+
+  private Integer sender;
+
+  public Integer receiver ;
+
+  public Integer amount;
+
+  public signature;
+
+    public Vector  myAccount;
+
+  public void newOperation() {
+  }
+
+}
---
+++ Transaction.java
@@ -0,0 +1,23 @@
+package BABY-BLOCKCHAIN;
+
+import Integer;
+import String;
+
+public class Transaction extends Hash {
+
+  public String TransactionID;
+
+  public ArrayList<operation> setOfOperation;
+
+  public Integer nonce;
+
+  public void public static Transaction createTransaction() {
+  }
+
+  public void public String toString() {
+  }
+
+  public void public void print () {
+  }
+
+}
